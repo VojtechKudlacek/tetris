5ab42069b8809d4c0233663a603a659622315c9d
Vojtech Kudlacek <vojtech.kudlacek@socialbakers.com>
2020-08-30 19:16:47 +0200

    More comments

--- src/tetris/managers/ControlManager.ts
+++ src/tetris/managers/ControlManager.ts
@@ -1,7 +1,9 @@
+/** Class to manage controls and register event listeners */
 class ControlManager {
 
+	/** Key to store custom user settings in local storage */
 	private readonly STORAGE_KEY: string = 'controls';
-
+	/** Default controls */
 	private readonly DEFAULT_CONTROLS: Controls = {
 		LEFT: 37, // Arrow left
 		RIGHT: 39, // Arrow right
@@ -12,28 +14,38 @@ class ControlManager {
 		ROTATE_RIGHT: 83, // S
 	};
 
+	/** Event listeners called for according events */
 	private listeners: Dictionary<ControlListener> = {};
-
+	/** Actual game controls */
 	public controls!: Controls;
 
+	/** Initialize constrols from custom keys or default ones */
 	private initControls(): void {
 		const customControls = localStorage.getItem(this.STORAGE_KEY);
 		if (customControls) {
+			// I believe that users doesn't modify the local storage. :)
 			this.controls = JSON.parse(customControls) as Controls;
 		} else {
 			this.controls = this.DEFAULT_CONTROLS;
 		}
 	}
 
+	/** Initialize used event listeners on whole window */
 	private initListeners(): void {
 		window.addEventListener('keydown', ({ keyCode }) => this.listeners.keydown && this.listeners.keydown(keyCode, this.controls));
 		window.addEventListener('keyup', ({ keyCode }) => this.listeners.keyup && this.listeners.keyup(keyCode, this.controls));
 	}
 
+	/**
+	 * Set listener for given event
+	 * @param event Event to be listened for
+	 * @param listener Listener function
+	 */
 	public setListener(event: AvailableEvent, listener: ControlListener): void {
 		this.listeners[event] = listener;
 	}
 
+	/** Initialize controls and listeners */
 	public init(): void {
 		this.initControls();
 		this.initListeners();
